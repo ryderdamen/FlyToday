cf726c1aa5332a32b2d0207c887b3165817695a7
Ryder <dev@ryderdamen.com>
2018-03-27 17:42:00 -0400

    Adding a privacy policy in prep for release on google actions

---
+++ privacy/index.php
@@ -0,0 +1,23 @@
+<?php
+	// Privacy Policy
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Fly Today - Privacy Policy</title>
+  </head>
+  <body>
+	  <h1>Fly Today - Privacy Policy</h1>
+	  Here's our super simple privacy policy.
+	  
+	  <h2>What information do you collect?</h2>
+	  <p>Automatically, we collect HTTP logs, data transmitted by Actions, and usage information, but no personal identification stuff.</p>
+	  
+	  <h2>How do you use the information?</h2>
+	  <p>We pretty much don’t. We’ll probably check in once in a while to see how many people are using the service, but other than that, nope.</p>
+	  
+	  <h2>What information do you share?</h2>
+	  <p>Nothing. Once again, it’s not that exciting, so we’ve not no plans to share any of this data any time soon.</p>
+  </body>
+</html>
