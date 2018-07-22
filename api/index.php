a274efe556eff294115e5b22d9104a6499df85a7
Ryder <dev@ryderdamen.com>
2018-07-22 15:48:51 -0400

    Updating to new webhook framework, api v2

--- api/index.php
+++ api/index.php
@@ -6,96 +6,58 @@
 	Author: Ryder Damen | ryderdamen.com
 */
 
+include('webhook.php');
+$wh = new Webhook('fly-today');
 
-// Lifecycle --------------------------------------------------------------------------------------------------------------
+// Get Parameters
+$airport = $wh->get_parameter('airport');
+$airportCode = $airport['ICAO'];
+$airportFullName = $airport['name'];
+$airportCity = $airport['city'];
 
-// Retrieve POST body from the input stream
-$json = file_get_contents('php://input');
-
-// If we don't have anything, it's not a _POST, so just die silently
-if ($json == '') {
-	header('Location: https://ryderdamen.com/projects/fly-today/');
-	die;
-}
-
-
-
-// Otherwise, let's decode this and grab some variables
-$action = json_decode($json, true);
-$airportCode = $action['result']['parameters']['airport']['ICAO'];
-$airportFullName = $action['result']['parameters']['airport']['name'];
-$airportCity = $action['result']['parameters']['airport']['city'];
-
-
-// Now, let's call the AVWX API and get the METAR data
+// Call the AVWX API and build a response for the user
 $metarJson = getMetar($airportCode);
 
-// Send a JSON header so Google knows what to expect
-header("Content-type:application/json");
-
-// If we don't hear from the AVWX API, let's throw an error message for the user
-if ($metarJson == '') {
-	echo buildErrorResponse();
+if ($metarJson == '') { // If there was an error retrieving the JSON
+	$errorText = "Sorry, something wen't wrong retrieving the weather data.";
+	$wh->build_simpleResponse($errorText, $errorText);
+	$wh->respond();
 	die;
 }
 
-// Otherwise, let's send the weather so the pilots can get flying
-echo buildResponse($metarJson, $airportCode, $airportFullName, $airportCity);
-
-
-// Functions --------------------------------------------------------------------------------------------------------------
-
-// Returns a general error message to dialogflow if something went wrong
-function buildErrorResponse() {
-	
-	$default = "Hmm, I'm having a bit of trouble right now. It's probably best to look it up the old fashioned way. Sorry!";
-	
-	return json_encode(array(
-		"speech" => $default,
-		"displayText" => $default,
-	));
+// If the name of the airport contains the name of the city, just use the name of the airport
+if ( strpos( strtolower($airportFullName), strtolower($airportCity)) !== false ) {
+	$response_airportName = $airportFullName;
+} else {
+	$response_airportName = $airportFullName . ", " . $airportCity; 
 }
 
-// Returns the weather info to the user
-function buildResponse($metarJson, $airportCode, $airportFullName, $airportCity) {
-		
-	// If the name of the airport contains the name of the city, just use the name of the airport
-	if ( strpos( strtolower($airportFullName), strtolower($airportCity)) !== false ) {
-		$response_airportName = $airportFullName;
-	}
-	else {
-		$response_airportName = $airportFullName . ", " . $airportCity; 
-	}
-	
-	// Say something based on whether it's VFR/IFR
-	switch ($metarJson['Flight-Rules']) {
-		case "LIFR":
-			$response_full = "It's Looking like low IFR right now at " . $response_airportName;
-			break;
-		case "SVFR":
-			$response_full = "It's Looking like special VFR right now at " . $response_airportName . ". Your call.";
-			break;
-		case "MVFR": 
-			$response_full = "It's Looking like marginal VFR right now at " . $response_airportName . ". Your call.";
-			break;
-		case "VFR":
-			$response_full = "Good news, it's VFR at " . $response_airportName . ". Let's go flying!";
-			break;
-		default:
-			$response_full = "It's Looking like " . $metarJson['Flight-Rules'] . " right now at " . $response_airportName . ". Your call.";
-	}
-	
-	return json_encode(array(
-		"speech" => $response_full,
-		"displayText" => $response_full,
-	));
-	
+// Say something based on whether it's VFR/IFR
+switch ($metarJson['Flight-Rules']) {
+	case "LIFR":
+		$response_text = "It's looking like low IFR right now at " . $response_airportName . " 🙁";
+		$response_speech = "It's looking like low IFR.";
+		break;
+	case "SVFR":
+		$response_text = "It's looking like special VFR right now at " . $response_airportName . ". Your call.";
+		$response_speech = "It's looking like special VFR.";
+		break;
+	case "MVFR": 
+		$response_text = "It's looking like marginal VFR right now at " . $response_airportName . ". Your call.";
+		$response_speech = "It's looking like marginal VFR.";
+		break;
+	case "VFR":
+		$response_text = "Good news, it's VFR at " . $response_airportName . ". Let's go flying! 🛩";
+		$response_speech = "Good news, it's VFR.";
+		break;
+	default:
+		$response_text = "It's Looking like " . $metarJson['Flight-Rules'] . " right now at " . $response_airportName . ". Your call.";
+		$response_speech = "It's Looking like " . $metarJson['Flight-Rules'] . " right now at " . $response_airportName . ". Your call.";
 }
 
-// Gets the flight rules from the JSON	
-function getFlightRules($metarJson) {
-	return $metarJson['Flight-Rules'];
-}
+$wh->build_simpleResponse($response_speech, $response_text);
+$wh->respond();
+
 
 // Gets the METAR from the API
 function getMetar($code) {
